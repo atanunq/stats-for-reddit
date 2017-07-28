09d33039f44314051ad0500bcab21c60965c4d91
Atanas Yankov <atanas.yankov98@gmail.com>
2017-07-28 15:10:38 +0300

    Add /keys and /authors

--- index.js
+++ index.js
@@ -7,9 +7,20 @@ const r = config.init();
 //Load view engine
 app.set('views', './views')
 app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-  getUpvotedCount(r.username,10,res)
+  showUpvoted(r.username,10,res)
+})
+app.get('/keys', function (req, res) {
+  r.getSubmission('5by1wy').fetch().then(post =>{
+    res.render('keys',{
+      post: post
+    })
+  })
+})
+app.get('/authors', function(req, res){
+  showUpvotedAuthors(r.username,20,res)
 })
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
@@ -27,12 +38,34 @@ function findComments(user){
   });
 }
 //findComments('spez');
+function showUpvotedAuthors(user, limit, res){
+  var authorNames = [];
+  r.getUser(user).getUpvotedContent({limit:limit}).then(upvoted => {
+    for (var i = 0; i < upvoted.length; i++) {
+      authorNames.push(upvoted[i].author.name);
+    }
+    var counts = {};
+    authorNames.forEach(function(x) { counts[x] = (counts[x] || 0)+1; });
+    var authors = [];
+    for (var author in counts) {
+        authors.push([author, counts[author]]);
+    }
+    authors.sort(function(a, b) {
+        return b[1] - a[1];
+    });
+    res.render('authors', {
+        user: user,
+        limit: limit,
+        authors: authors
+      });
+  })
+}
 
-function getUpvotedCount(user, limit, res){
+function showUpvoted(user, limit, res){
   var subredditNames = [];
-  r.getUser(user).getUpvotedContent({limit: limit}).then(value => {
-    for (var i = 0; i < value.length; i++) {
-      subredditNames.push(value[i].subreddit_name_prefixed);
+  r.getUser(user).getUpvotedContent({limit: limit}).then(upvoted => {
+    for (var i = 0; i < upvoted.length; i++) {
+      subredditNames.push(upvoted[i].subreddit_name_prefixed);
     }
     var counts = {};
     subredditNames.forEach(function(x) { counts[x] = (counts[x] || 0)+1; });
@@ -46,8 +79,7 @@ function getUpvotedCount(user, limit, res){
     res.render('index', {
         subreddits: subreddits,
         user: user,
-        limit: limit,
-        value: value[0]
+        limit: limit
       });
   });
 }
