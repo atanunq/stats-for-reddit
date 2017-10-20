331af086799f73ec1e63d2fb02320b6ca6044543
Atanas Yankov <atanas.yankov98@gmail.com>
2017-10-20 18:52:15 +0100

    Separate routes from index.js

---
+++ routes.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const app = express();
+const routesCtrl = require('./routesController');
+var instancePromise = null;
+const snoowrap = require('snoowrap');
+module.exports = function(app){
+  app.use(function (req, res, next) {
+    if(instancePromise || req.originalUrl == '/authenticate' || req.query.state == "fe211bebc52eb3da9bef8db6e63104d3") next()
+    //TODO: redirect unauthenticated users
+    else res.render("index", {
+      doNotShowHeaders: true
+    })
+  })
+  app.get('/', function (req, res) {
+    try{
+      instancePromise.then(r => {
+        r.getMe().then(user => {
+          routeCtrl.showUpvoted(user,50,res)
+        })
+      })
+    }
+    catch (e) {
+      res.render('index');
+    }
+  })
+  app.get('/upvoted', function (req, res) {
+    instancePromise.then(r => {
+      r.getMe().then(user => {
+        routeCtrl.showUpvoted(user,50,res)
+      })
+    })
+  })
+  app.get('/keys', function (req, res) {
+    instancePromise.then(r => {
+      r.getSubmission('750jsa').fetch().then(post => {
+        res.render('keys',{
+          post: post
+        })
+      })
+    })
+  })
+  app.get('/charts', function(req, res) {
+    instancePromise.then(r => {
+      res.render('charts');
+    })
+  })
+  app.get('/authors', function(req, res){
+    instancePromise.then(r => {
+      r.getMe().then(user => {
+        routeCtrl.showUpvotedAuthors(user,50,res)
+      })
+    })
+  })
+  app.get('/sandbox', function(req, res){
+    instancePromise.then(r => {
+      r.getSubmission('74f01f').fetch().then(post => {
+        if (!post.media) {
+          post.url = changeGifvExtention(post.url);
+          post.embedPost = false;
+        } else {
+          post.embedPost = true;
+        }
+        res.render('sandbox', {
+          post: post
+        })
+      })
+    })
+  })
+
+  const clientId = process.env.REDDIT_CLIENT_ID;
+  const clientSecret = process.env.REDDIT_CLIENT_SECRET;
+
+  app.get('/authenticate', function(req, res){
+      var authenticationUrl = snoowrap.getAuthUrl({
+        clientId: clientId,
+        scope: ['identity history read'],
+        redirectUri: 'http://localhost:3000/success',
+        permanent: false,
+        state: 'fe211bebc52eb3da9bef8db6e63104d3'
+    });
+    res.redirect(authenticationUrl);
+  })
+  app.get('/success', function (req, res) {
+    var code = req.query.code;
+    instancePromise = snoowrap.fromAuthCode({
+      code: code,
+      userAgent: 'stats-for-reddit 1.0.0 by u/atanunq',
+      clientId: clientId,
+      clientSecret: clientSecret,
+      redirectUri: 'http://localhost:3000/success'
+    })
+    res.render("success");
+  })
+}
---
+++ routesController.js
@@ -0,0 +1,94 @@
+const moment = require('moment');
+function changeGifvExtention(url) {
+    // TODO: find a fix for the v.reddit videos
+    var parts = url.split(".");
+    if (parts[parts.length - 1] == "gifv") {
+        parts[parts.length - 1] = "gif";
+    }
+    return parts.join('.');
+}
+function showUpvotedAuthors(user, limit, res){
+  var authorNames = [];
+  user.getUpvotedContent({limit:limit}).then(upvoted => {
+    for (var i = 0; i < upvoted.length; i++) {
+      authorNames.push(upvoted[i].author.name);
+    }
+    var counts = {};
+    authorNames.forEach(function(x) { counts[x] = (counts[x] || 0)+1; });
+    var authors = [];
+    for (var author in counts) {
+        authors.push([author, counts[author]]);
+    }
+    authors.sort(function(a, b) {
+        return b[1] - a[1];
+    });
+    res.render('authors', {
+        user: user,
+        limit: limit,
+        authors: authors
+      });
+  })
+}
+
+function showUpvoted(user, limit, res){
+  var subredditNames = [];
+  user.getUpvotedContent({limit: limit}).then(upvoted => {
+    for (var i = 0; i < upvoted.length; i++) {
+      subredditNames.push({
+        subreddit_name: upvoted[i].subreddit_name_prefixed,
+        post: upvoted[i]
+      });
+    }
+    var subreddits = [];
+    subredditNames.forEach(function(x) {
+      var found = false;
+      for(var i = 0; i < subreddits.length; i++) {
+          if (subreddits[i].subreddit_name == x.subreddit_name) {
+              found = true;
+              subreddit = subreddits[i];
+          }
+      }
+      if(found){
+        subreddit.count ++;
+        subreddit.posts.push(x.post)
+      } else {
+        subreddits.push({
+          subreddit_name: x.subreddit_name,
+          count: 1,
+          posts: [x.post]
+        })
+      }
+    });
+    subreddits.sort(function (a, b) {
+        return b.count - a.count;
+    });
+    // add postedAgo and postedDate properties to every element
+    for(var i = 0; i < subreddits.length; i++) {
+      for(var j = 0;j < subreddits[i].posts.length; j++){
+        var currentElement = subreddits[i].posts[j];
+				var day = moment.unix(currentElement.created_utc);
+        currentElement.postedAgo = day.fromNow(false,'d');
+        currentElement.postedDate = day.format("DD MMMM YYYY");
+          // change every .gifv extention to .gif
+				if (!currentElement.media) {
+						currentElement.url = changeGifvExtention(currentElement.url);
+						currentElement.embedPost = false;
+				} else {
+						currentElement.embedPost = true;
+				}
+      }
+    }
+    //console.log(subreddits[0].posts[0].subreddit_name_prefixed);
+
+    //console.log(subreddits['r/Dota2'])
+    res.render('upvoted', {
+        subreddits: subreddits,
+        user: user,
+        limit: limit
+      });
+  });
+}
+module.exports = {
+  showUpvotedAuthors: showUpvotedAuthors,
+  showUpvoted: showUpvoted
+}
