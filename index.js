b9c28a9fbcd4c0ecb67ad98b295f352865cf6c4d
Atanas Yankov <atanas.yankov98@gmail.com>
2017-10-21 01:11:00 +0100

    Output sample chart

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 app.set('views', './views')
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
+app.use('/scripts', express.static(__dirname + '/node_modules/Chart.js/dist/'));
 var routes = require('./routes')(app);
 
 app.listen(3000, function () {
