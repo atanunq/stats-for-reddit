5b6f00adf2bb33b5aa288e676bbb4015a80da12f
Atanas Yankov <atanas.yankov98@gmail.com>
2017-10-22 23:57:17 +0100

    Test heroku deploy

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 app.use('/scripts', express.static(__dirname + '/node_modules/Chart.js/dist/'));
 var routes = require('./routes')(app);
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port + '!')
 })
