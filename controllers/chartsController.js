31fa275b44aff99cdb4fac93f9698739e478f070
Atanas Yankov <atanas.yankov98@gmail.com>
2017-10-28 12:34:43 +0100

    Explain functionality with comments

--- controllers/chartsController.js
+++ controllers/chartsController.js
@@ -1,13 +1,18 @@
 function createChart(data, limit, res){
+  // variables
   var labels = [];
   var count = [];
   var backgroundColor = [];
   var hoverBackgroundColor = [];
   var borderColor = [];
+
+  // loop through the array of subreddits and filter them out if they apper only once
   for(var i=0;i<data.length;i++){
     if(data[i].count > 1){
       labels.push(data[i].subreddit_name);
       count.push(data[i].count);
+
+      // generate random colors for the charts
       rgb = [
         Math.random()*255,
         Math.random()*255,
@@ -18,6 +23,8 @@ function createChart(data, limit, res){
       borderColor.push("rgba(" + rgb[0] + "," + rgb[1] + "," + rgb[2] + ", 1)");
     }
   }
+
+  // configure the chart
   var chartData = {
     labels: labels,
     datasets: [{
@@ -46,11 +53,13 @@ function createChart(data, limit, res){
     }
   };
 
+  // render the charts view
   res.render('charts',{
     data: JSON.stringify(chartData),
     options: JSON.stringify(options)
   })
 }
+// export the function from the controller
 module.exports = {
   createChart: createChart
 }
--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-const request = require('request');
 const express = require('express');
 const app = express();
+
 //Load view engine
 app.set('views', './views')
 app.set('view engine', 'pug');
+
+//folder structure
 app.use(express.static(__dirname + '/public'));
-app.use('/scripts', express.static(__dirname + '/node_modules/Chart.js/dist/'));
+app.use('/scripts', express.static(__dirname + '/node_modules/chart.js/dist/'));
+
+//import routing file and run it with the app variable
 var routes = require('./routes')(app);
+
+// check for PORT env variable or 3000 for localhost
 var port = process.env.PORT || 3000;
+
+// run the app
 app.listen(port, function () {
   console.log('Example app listening on port ' + port + '!')
 })
